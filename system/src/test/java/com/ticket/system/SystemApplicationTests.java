cfc5f4653f6aeceec4ca5b2940c4901bda56862d
WorldWideWest <dzeno.dzafic@windowslive.com>
2022-01-22 13:51:03 +0100

    Initialization commit

---
+++ system/src/test/java/com/ticket/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
