d4a35cb529584817645ca60e2b7147503b01e054
WorldWideWest <dzeno.dzafic@windowslive.com>
2022-01-23 18:40:25 +0100

    Adding support for database user registration and authentication

--- sql/table_init.sql
+++ sql/table_init.sql
@@ -3,8 +3,9 @@ CREATE DATABASE IF NOT EXISTS car_repair;
 
 USE car_repair;
 
-DROP TABLE IF EXISTS Ticket;
+-- Tables for the webapp functionality
 
+DROP TABLE IF EXISTS Ticket;
 CREATE TABLE IF NOT EXISTS Ticket(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(200) NOT NULL,
@@ -14,9 +15,7 @@ CREATE TABLE IF NOT EXISTS Ticket(
     repair_type VARCHAR(1000) NOT NULL
 )ENGINE=INNODB;
 
-
 DROP TABLE IF EXISTS `Status`;
-
 CREATE TABLE IF NOT EXISTS `Status`(
 	id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     ticket INT,
@@ -25,4 +24,33 @@ CREATE TABLE IF NOT EXISTS `Status`(
     cost DOUBLE,
     FOREIGN KEY(ticket) REFERENCES Ticket(id)
 )ENGINE=INNODB;
+
+
+-- Tables for authentication
+USE car_repair;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(
+	username VARCHAR(50) NOT NULL PRIMARY KEY,
+    `password` VARCHAR(50) NOT NULL,
+    enabled TINYINT(1) NOT NULL
+)ENGINE=INNODB;
+
+INSERT INTO users VALUES
+('dzenan.dzafic', '{noop}eminem662', 1);
+
+
+DROP TABLE IF EXISTS authorities;
+CREATE TABLE IF NOT EXISTS authorities(
+	username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    
+    UNIQUE KEY `authorities_idx_1` (`username`, `authority`),    
+    CONSTRAINT  `authorities_ibfk_1` FOREIGN KEY (username) REFERENCES users(username)
+);
+
+INSERT INTO authorities VALUES
+('dzenan.dzafic', 'ROLE_EMPLOYEE');
+
+
 
--- system/src/main/java/com/ticket/system/config/SystemSecurityConfig.java
+++ system/src/main/java/com/ticket/system/config/SystemSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.ticket.system.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,13 +14,21 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @Configuration @EnableWebSecurity
 public class SystemSecurityConfig extends WebSecurityConfigurerAdapter{
 
+    // Adding reference to security data source
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-        UserBuilder users = User.withDefaultPasswordEncoder();
+        // UserBuilder users = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-            .withUser(users.username("dzenan.dzafic").password("eminem662").roles("MANAGER"));
+        // auth.inMemoryAuthentication()
+        //     .withUser(users.username("dzenan.dzafic").password("eminem662").roles("MANAGER"));
+
+        // JPA -authentication
+
+        auth.jdbcAuthentication().dataSource(dataSource);
 
     }
 
